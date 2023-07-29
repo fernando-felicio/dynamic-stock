52c7de09630dbb7302e7dee45ef820f1a3ca5c72
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-29 20:07:25 -0300

    imprimindo saida de dados do estoque

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,10 @@ import java.util.Locale;
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
+		
+		//Recebendo os inputs do usuário
 		
 		System.out.printf("Enter product information:%n==================%n");
 		
@@ -19,6 +22,10 @@ public class Program {
 		
 		System.out.println("Amount: ");
 		int productAmount = sc.nextInt();
+		
+		//Retornando os dados do estoque após input
+		System.out.printf("The quantity of %s in stock is %d and the price is R$%.2f", productName, productAmount, productPrice);
+		
 		
 		
 		sc.close();
