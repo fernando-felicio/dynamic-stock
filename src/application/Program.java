08c3b0476903394a8c579026c067b272d3a664af
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-29 19:45:18 -0300

    importando bibliotecas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,19 @@
 package application;
 
+import java.util.Scanner;
+
+import java.util.Locale;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		
+		
+		
+		
+		sc.close();
 
 	}
 
