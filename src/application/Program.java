543d08716185df294b6c9db96120b7a2268b7116
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-29 19:56:18 -0300

    Tratando inputs do usuario

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,8 +9,16 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		
+		System.out.printf("Enter product information:%n==================%n");
 		
+		System.out.println("Product's name: ");
+		String productName = sc.nextLine();
 		
+		System.out.println("Price of the product: R$");
+		double productPrice = sc.nextDouble();
+		
+		System.out.println("Amount: ");
+		int productAmount = sc.nextInt();
 		
 		
 		sc.close();
