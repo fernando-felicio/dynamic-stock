a5b8b3649f513d0b77b7d0f2377ecad78f77dab3
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-29 20:15:12 -0300

    Tratando todos os dados de entrada

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,26 +8,41 @@ public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner (System.in);
-		
-		//Recebendo os inputs do usuário
-		
+		Scanner sc = new Scanner(System.in);
+
+		// Recebendo os inputs do usuário
+
 		System.out.printf("Enter product information:%n==================%n");
-		
+
 		System.out.println("Product's name: ");
 		String productName = sc.nextLine();
-		
+
 		System.out.println("Price of the product: R$");
 		double productPrice = sc.nextDouble();
-		
+
 		System.out.println("Amount: ");
 		int productAmount = sc.nextInt();
-		
-		//Retornando os dados do estoque após input
-		System.out.printf("The quantity of %s in stock is %d and the price is R$%.2f", productName, productAmount, productPrice);
-		
-		
-		
+
+		// Retornando os dados do estoque após input
+		System.out.printf("The quantity of %s in stock is %d and the price is R$%.2f%n==========%n", productName,
+				productAmount, productPrice);
+
+		// Input para adicionar itens ao estoque
+		System.out.println("Enter the number of products to be added in stock: ");
+		int productAdd = sc.nextInt();
+
+		// Retornando os dados do estoque após adição de itens
+		System.out.printf("Updated data: %nThe quantity of %s in stock is %d and the price is R$%.2f%n==========%n",
+				productName, productAmount, productPrice);
+
+		// Input para remover itens do estoque
+		System.out.println("Enter the number of products to be removed in stock: ");
+		int productRemove = sc.nextInt();
+
+		// Retornando os dados do estoque após remoção de itens
+		System.out.printf("Updated data: %nThe quantity of %s in stock is %d and the price is R$%.2f%n",
+				productName, productAmount, productPrice);
+
 		sc.close();
 
 	}
